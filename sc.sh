10c69e6d0111aa8c795ceb471d8059147dcbdb20
bsz <bszlacht@student.agh.edu.pl>
2023-03-31 12:55:16 +0200

    sadasd

--- sc.sh
+++ sc.sh
@@ -5,4 +5,6 @@
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr23-cpu
 ./setup-environment.sh
+curl https://cran.r-project.org/src/base/R-4/R-4.2.3.tar.gz -o R-4.2.3.tar.gz
+tar xf R-4.2.3.tar.gz
 R --no-save < start.R
