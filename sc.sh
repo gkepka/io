992732a5e49ffaf24d140ea22a808ac1f97d8385
bsz <bszlacht@student.agh.edu.pl>
2023-04-14 11:02:46 +0200

    asda

--- sc.sh
+++ sc.sh
@@ -6,5 +6,13 @@
 #SBATCH --account=plgmpr23-cpu
 
 module add scipy-bundle/2021.10-foss-2021b
+module add r/4.2.0-foss-2021b
+
 chmod +x ./start.R
 R --no-save < start.R
+
+
+
+
+# ./setup-environment.sh
+# R --no-save < start.R
